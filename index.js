1cb1c902dd7e6b43fcb1ef738f18d18015e4b6c5
dotnetnat@gmail.com <dotnetnat@gmail.com>
2018-06-25 20:09:17 +0700

    fixed no resize

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 import {
   View,
   Image,
   StyleSheet,
   TouchableOpacity,
   Platform
-} from 'react-native'
-import ImagePicker from 'react-native-image-picker'
-import ImageResizer from 'react-native-image-resizer'
-import RNFS from 'react-native-fs'
+} from "react-native";
+import ImagePicker from "react-native-image-picker";
+import ImageResizer from "react-native-image-resizer";
+import RNFS from "react-native-fs";
 
 export default class PhotoUpload extends React.Component {
   static propTypes = {
@@ -26,105 +26,108 @@ export default class PhotoUpload extends React.Component {
     onCancel: PropTypes.func, // when user cancel
     onResponse: PropTypes.func, // on response exists!
     onRender: PropTypes.func, // after render
-    onResizedImageUri: PropTypes.func, // when image resized is ready
-  }
+    onResizedImageUri: PropTypes.func // when image resized is ready
+  };
 
   state = {
     height: this.props.height || 300,
     width: this.props.width || 300,
-    format: this.props.format || 'JPEG',
+    format: this.props.format || "JPEG",
     quality: this.props.quality || 80,
     buttonDisabled: false
-  }
+  };
 
   options = {
-    title: this.props.pickerTitle || 'Select Photo',
+    title: this.props.pickerTitle || "Select Photo",
     storageOptions: {
       skipBackup: true,
-      path: 'images'
+      path: "images"
     }
-  }
+  };
 
   openImagePicker = () => {
-    this.setState({buttonDisabled: true})
-    if (this.props.onStart) this.props.onStart()
+    this.setState({ buttonDisabled: true });
+    if (this.props.onStart) this.props.onStart();
 
     // get image from image picker
     ImagePicker.showImagePicker(this.options, async response => {
-      this.setState({buttonDisabled: false})
+      this.setState({ buttonDisabled: false });
       //console.log('Response = ', response)
-      let rotation = 0
-      const {originalRotation} = response
+      let rotation = 0;
+      const { originalRotation } = response;
 
-
-      if (this.props.onResponse) this.props.onResponse(response)
+      if (this.props.onResponse) this.props.onResponse(response);
 
       if (response.didCancel) {
-        console.log('User cancelled image picker')
-        if (this.props.onCancel) this.props.onCancel('User cancelled image picker')
-        return
+        console.log("User cancelled image picker");
+        if (this.props.onCancel)
+          this.props.onCancel("User cancelled image picker");
+        return;
       } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error)
-        if (this.props.onError) this.props.onError(response.error)
-        return
+        console.log("ImagePicker Error: ", response.error);
+        if (this.props.onError) this.props.onError(response.error);
+        return;
       } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton)
-        if (this.props.onTapCustomButton) this.props.onTapCustomButton(response.customButton)
-        return
+        console.log("User tapped custom button: ", response.customButton);
+        if (this.props.onTapCustomButton)
+          this.props.onTapCustomButton(response.customButton);
+        return;
       }
 
-      let { height, width, quality, format } = this.state
+      let { height, width, quality, format } = this.state;
 
       //Determining rotation param
-      if ( originalRotation === 90) {
-        rotation = 90
+      if (originalRotation === 90) {
+        rotation = 90;
       } else if (originalRotation === 180) {
         //For a few images rotation is 180.
-        rotation = -180
-      } else if ( originalRotation === 270 )  {
+        rotation = -180;
+      } else if (originalRotation === 270) {
         //When taking images with the front camera (selfie), the rotation is 270.
-        rotation = -90
+        rotation = -90;
       }
       // resize image
       const resizedImageUri = await ImageResizer.createResizedImage(
         `data:image/jpeg;base64,${response.data}`,
-        height,
-        width,
+        response.height,
+        response.width,
         format,
         quality,
         rotation
-      )
+      );
 
-      if (this.props.onResizedImageUri) this.props.onResizedImageUri(resizedImageUri)
+      if (this.props.onResizedImageUri)
+        this.props.onResizedImageUri(resizedImageUri);
 
-      const filePath = Platform.OS === 'android' && resizedImageUri.uri.replace
-        ? resizedImageUri.uri.replace('file:/data', '/data')
-        : resizedImageUri.uri
+      const filePath =
+        Platform.OS === "android" && resizedImageUri.uri.replace
+          ? resizedImageUri.uri.replace("file:/data", "/data")
+          : resizedImageUri.uri;
 
       // convert image back to base64 string
-      const photoData = await RNFS.readFile(filePath, 'base64')
-      let source = { uri: resizedImageUri.uri }
+      const photoData = await RNFS.readFile(filePath, "base64");
+      let source = { uri: resizedImageUri.uri };
       this.setState({
         avatarSource: source
-      })
+      });
 
       // handle photo in props functions as data string
-      if (this.props.onPhotoSelect) this.props.onPhotoSelect(photoData)
-    })
-  }
+      if (this.props.onPhotoSelect) this.props.onPhotoSelect(photoData);
+    });
+  };
 
   renderChildren = props => {
     return React.Children.map(props.children, child => {
       if (child.type === Image && this.state.avatarSource) {
         return React.cloneElement(child, {
           source: this.state.avatarSource
-        })
-      } else return child
-    })
-  }
+        });
+      } else return child;
+    });
+  };
 
   componentDidUpdate() {
-    if (this.props.onAfterRender) this.props.onAfterRender(this.state)
+    if (this.props.onAfterRender) this.props.onAfterRender(this.state);
   }
 
   render() {
@@ -137,14 +140,14 @@ export default class PhotoUpload extends React.Component {
           {this.renderChildren(this.props)}
         </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   }
-})
+});
